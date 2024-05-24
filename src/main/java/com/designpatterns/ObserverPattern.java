43978c7f90b67f27c595238e8b21f49a73198958
Arpit Kumar Pandey <arpipand@cisco.com>
2024-05-25 00:33:24 +0530

    feat: full implementation of observer design pattern

--- src/main/java/com/designpatterns/ObserverPattern.java
+++ src/main/java/com/designpatterns/ObserverPattern.java
@@ -1,50 +1,150 @@
 package com.designpatterns;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 import lombok.Getter;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ObserverPattern {
-    interface Subject {
+    public interface Subject {
         public void registerObserver(Observer observer);
 
         public void removeObserver(Observer observer);
 
-        public void notifyObserver();
+        public void notifyObservers();
     }
 
-    interface Observer {
+    public interface Observer {
+        public String getName();
+
         public void update(float temperature);
     }
 
-    class WeatherMeasurements implements Subject {
+    public static class CurrentConditionsDisplay implements Observer {
+        @Override
+        public String getName() {
+            return "CurrentConditionsDisplay";
+        }
+
+        @Override
+        public void update(float temperature) {
+            log.info("updated temperature to [{}] in display: [{}]", temperature, getName());
+        }
+    }
+
+    public static class ForecastDisplay implements Observer {
+        @Override
+        public String getName() {
+            return "ForecastDisplay";
+        }
+
+        @Override
+        public void update(float temperature) {
+            log.info("updated temperature to [{}] in display: [{}]", temperature, getName());
+        }
+    }
+
+    public static class StatisticsDisplay implements Observer {
+        @Override
+        public String getName() {
+            return "StatisticsDisplay";
+        }
+
+        @Override
+        public void update(float temperature) {
+            log.info("updated temperature to [{}] in display: [{}]", temperature, getName());
+        }
+    }
+
+    public static class WeatherMeasurements implements Subject {
         @Getter
-        @Setter
-        float temperature;
+        private float temperature = -1;
+        private Set<Observer> displays = new HashSet<Observer>();
 
-        List<Observer> observers;
+        WeatherMeasurements() {
+            log.info("initialized WeatherMeasurements with temperature: [{}] and no displays", temperature);
+        }
 
-        @Override
-        public void registerObserver(Observer observer) {
-            observers.add(observer);
+        WeatherMeasurements(float temperature) {
+            this.temperature = temperature;
+        }
+
+        WeatherMeasurements(Set<Observer> displays) {
+            this.displays = displays;
+        }
+
+        public void setTemperature(float newTemperature) {
+            if (newTemperature != this.temperature) {
+                this.temperature = newTemperature;
+                this.measurementsChanged();
+            }
         }
 
         @Override
-        public void removeObserver(Observer observer) {
-
+        public void registerObserver(Observer display) {
+            if (display == null) {
+                throw new NullPointerException("display cannot be null");
+            }
+            if (displays.contains(display)) {
+                log.info("Display: [{}] is already registered", display.getName());
+                return;
+            }
+            displays.add(display);
+            log.info("added observer: [{}] to weather measurement broadcast", display.getName());
         }
 
         @Override
-        public void notifyObserver() {
-            for (Observer observer : observers) {
-                observer.update(1);
+        public void removeObserver(Observer display) {
+            if (display == null) {
+                throw new NullPointerException("display cannot be null");
+            }
+            if (displays.contains(display)) {
+                displays.remove(display);
+                log.info("removed display: [{}] from weather measurement broadcast", display.getName());
+            } else {
+                throw new NoSuchElementException(
+                        String.format("Display %s is not currently displaying weather measurements",
+                                display.getName()));
+            }
+        }
+
+        @Override
+        public void notifyObservers() {
+            for (Observer observer : displays) {
+                observer.update(getTemperature());
             }
         }
 
         public void measurementsChanged() {
-
+            log.info("Measurements Changed! new temperature = [{}]", getTemperature());
+            notifyObservers();
         }
+    }
 
+    public static void main(String[] args) {
+        WeatherMeasurements subject = new WeatherMeasurements();
+        Observer ccd = new CurrentConditionsDisplay(), sd = new StatisticsDisplay(), fd = new ForecastDisplay();
+        subject.registerObserver(ccd);
+        subject.registerObserver(sd);
+        sleep(500);
+        subject.setTemperature(42);
+        subject.removeObserver(sd);
+        subject.registerObserver(fd);
+        sleep(500);
+        subject.setTemperature(30);
+        sleep(500);
+        subject.setTemperature(16);
+
+    }
+
+    private static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception e) {
+            log.warn(e.getMessage());
+        }
     }
 }
--- src/test/java/com/designpatterns/ObserverPatternTest.java
+++ src/test/java/com/designpatterns/ObserverPatternTest.java
@@ -1,12 +1,35 @@
 package com.designpatterns;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.designpatterns.ObserverPattern.CurrentConditionsDisplay;
+import com.designpatterns.ObserverPattern.ForecastDisplay;
+import com.designpatterns.ObserverPattern.StatisticsDisplay;
+import com.designpatterns.ObserverPattern.Subject;
+import com.designpatterns.ObserverPattern.WeatherMeasurements;
+
 public class ObserverPatternTest {
+    Subject subject;
+
+    @BeforeEach
+    public void setUp() {
+        subject = new WeatherMeasurements();
+    }
+
     @Test
-    public void testBasic() {
-        assertTrue(true);
+    public void testNullPointerException() {
+        assertThrows(NullPointerException.class, () -> {
+            subject.registerObserver(null);
+        });
+    }
+
+    @Test
+    public void testAddingObservers() {
+        subject.registerObserver(new CurrentConditionsDisplay());
+        subject.registerObserver(new StatisticsDisplay());
+        subject.registerObserver(new ForecastDisplay());
     }
 }
